024fb37ad7743310da0268f6f09e9a1de4dbd90e
ehwuts matsu <tsukan.git@gmail.com>
2016-12-10 23:07:59 -0800

    first run attempt

--- srchat.js
+++ srchat.js
@@ -1,1 +1,16 @@
+const config = require("./srchat-config.js");
+
 const Eris = require("eris");
+
+var bot = new Eris(config.discord_token);
+
+bot.on("ready", () => {
+	console.log("Ready.");
+});
+
+bot.on("messageCreate", (msg) => {
+	console.log("<" + msg.channel + ">" + msg.user  + ": " + 
+msg.content);
+});
+
+bot.connect();
