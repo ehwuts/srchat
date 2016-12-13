82d706ecdfa903f21dbb58b4367ddae1e2a0b0d2
ehwuts matsu <tsukan.git@gmail.com>
2016-12-12 21:12:11 -0800

    finish main web handling

--- srchat-webhandler.js
+++ srchat-webhandler.js
@@ -12,7 +12,14 @@ var web_options = {
 	}
 };
 
+var the_source_of_instability = undefined;
+
 function WebHandler(setkey, settimeout, wkey, waddr, wpath) {
+	if (the_source_of_instability !== undefined) {
+		console.log(":err second call to the fragility that is webhandler");
+		return the_source_of_instability;
+	};
+
 	this.lastmsg = 0;
 	this.ready = false;
 	this.key = setkey;
@@ -21,21 +28,41 @@ function WebHandler(setkey, settimeout, wkey, waddr, wpath) {
 	this.muted = false;
 	this.interval = undefined;
 	
-	this.respondlerLabels = [];
+	this.respondlerLabels = ["all"];
 	this.respondlers = [];
+	this.respondlers["all"] = (v) => {
+		var i = 0;
+		while (i < this.respondlerLabels.length) {
+			if (this.respondlerLabels[i] != "all" && this.respondlers[this.respondlerLabels[i]] !== undefined)
+				this.respondlers[this.respondlerLabels[i]](v);
+			i++;
+		}
+	};
 	
 	web_key = wkey;
 	web_addr = waddr;
 	web_options.hostname = waddr;
 	web_options.path = wpath;
+	
+	the_source_of_instability = this;
 }
 
 WebHandler.prototype.setRespondler = function(label, stringsender) {
-	this.respondlers[label] = stringsender;
-	if (!this.respondlerLabels.includes(label)) this.respondlerLabels[label] = label;
+	if (label == "all") {
+		console.log(":err won't override web responder 'all'");
+		return;
+	}
+	
+	the_source_of_instability.respondlers[label] = stringsender;
+	if (!the_source_of_instability.respondlerLabels.includes(label)) the_source_of_instability.respondlerLabels.push(label);
 };
 WebHandler.prototype.unsetRespondler = function(label) {
-	if (this.respondlers[label] !== undefined) this.respondlers[label] = undefined;
+	if (label == "all") {
+		console.log(":err won't unset web responder 'all'");
+		return;
+	}
+	
+	if (the_source_of_instability.respondlers[label] !== undefined) the_source_of_instability.respondlers[label] = undefined;
 }
 //timeout and callback optional
 WebHandler.prototype.sendRequest = function(action, content, timeout, callback) {
@@ -48,18 +75,52 @@ WebHandler.prototype.sendRequest = function(action, content, timeout, callback) 
 	options.headers["Content-Length"] = Buffer.byteLength(postData);
 	if (timeout !== undefined) options.timeout = timeout;
 	
-	var req = (callback === undefined ? http.request(options) );
+	var req = (callback === undefined ? http.request(options) : http.request(options, callback) );
 	
 	req.on("error", (e) => { console.log(":err " + e.message); });
 	req.write(postData);
 	req.end();
 }
-WebHandler.prototype.readWeb = function () {
 
+function TryParseJson(js) {
+	try { return JSON.parse(js); }
+	catch(e) { return null; }
+}
+
+WebHandler.prototype.relayData = function (js) {
+	//console.log(js);
+	var d = TryParseJson(js);
+	if (d === null) {
+		the_source_of_instability.ready = false;
+		return;
+	}
+	
+	the_source_of_instability.lastmsg = d.last;
+	if (!the_source_of_instability.ready) {
+		the_source_of_instability.ready = true;
+	} else {
+		var i = 0;
+		while (i < d.lines.length) {
+			console.log("[w] " + d.lines[i]);
+			the_source_of_instability.respondlers[d.side](d.lines[i]);
+			++i;
+		}
+	}
+}
+WebHandler.prototype.receiveFunc = function (r) {
+	r.setEncoding("utf8");
+	r.blobHope = "";
+	r.on("data", (c) => { r.blobHope += c; });
+	r.on("end", () => { the_source_of_instability.relayData(r.blobHope); });
+}
+WebHandler.prototype.readWeb = function () {
+	//console.log(":readweb w/ last = "+the_source_of_instability.lastmsg);
+	if (the_source_of_instability.ready) the_source_of_instability.sendRequest("get", ""+the_source_of_instability.lastmsg, the_source_of_instability.timeout, the_source_of_instability.receiveFunc);
+	else the_source_of_instability.sendRequest("last", "", the_source_of_instability.timeout, the_source_of_instability.receiveFunc);
 }
 WebHandler.prototype.start = function (i) {
-	if (this.interval !== undefined) clearInterval(this.interval);
-	this.interval = setInterval(this.readWeb, i);
+	if (the_source_of_instability.interval !== undefined) clearInterval(the_source_of_instability.interval);
+	the_source_of_instability.interval = setInterval(the_source_of_instability.readWeb, i);
 }
 
 module.exports = WebHandler;
--- srchat.js
+++ srchat.js
@@ -2,7 +2,7 @@ const config = require("./srchat-config.js");
 
 const WebHandler = require("./srchat-webhandler.js");
 var web = new WebHandler(config.web_key, config.web_timeout, config.web_key, config.web_host, config.web_path);
-var web.start(config.web_interval);
+web.start(config.web_interval);
 
 const Eris = require("eris");
 var discord = new Eris(config.discord_token);
@@ -10,7 +10,7 @@ var discord = new Eris(config.discord_token);
 
 discord.on("ready", () => {
 	console.log(":Discord connection ready.");
-	web.setRespondler("discord", function (v) {discord.createMessage(config.discord_channel, v);});
+	web.setRespondler("discord", function (v) {discord.createMessage(config.discord_channel, v.replace("","**").replace("","__").replace("","*"));});
 });
 
 discord.on("messageCreate", (msg) => {
@@ -18,9 +18,9 @@ discord.on("messageCreate", (msg) => {
 		if (msg.content === "!test") {
 			discord.createMessage(msg.channel.id, "nou");
 		} else if (msg.content === "!who") {
-			web.sendRequest("who", "discord");
+			web.sendRequest("who", "discord", web.timeout, web.receiveFunc);
 		} else {
-			console.log("[DSC] " + msg.author.username + ": " + msg.content);
+			console.log("[d] " + msg.author.username + ": " + msg.content);
 			web.sendRequest("msg", "[d] " + msg.author.username + ": " + msg.content);
 		}
 	}
